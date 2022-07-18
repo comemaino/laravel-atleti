022d0baf00a3843e34a91d5570f4725bdfbc4472
comemaino <mat.maino@gmail.com>
2022-07-18 14:51:46 +0200

    Add: Admin/HomeController

--- app/Http/Controllers/Admin/AthleteController.php
+++ app/Http/Controllers/Admin/AthleteController.php
@@ -2,10 +2,98 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Athlete;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class AthleteController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate($this->getValidationRules());
+        $data = $request->all();
+        $athlete = Athlete::findOrFail($id);
+        $athlete->update($data);
+
+        return redirect()->route('admin.athletes.show', ['athlete' => $athlete->id]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    private function getValidationRules()
+    {
+        return [
+            'name' => 'required',
+            'genre' => 'required',
+        ];
+    }
 }

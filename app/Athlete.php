978bc3e73be275ab11eb5257870dc3322dea1689
comemaino <mat.maino@gmail.com>
2022-07-18 11:03:42 +0200

    Add: relations between athletes, countries, categories

--- app/Athlete.php
+++ app/Athlete.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Athlete extends Model
 {
-    //
+
+    protected $fillable = [
+        'name',
+        'genre'
+    ];
+
+    public function country()
+    {
+        return $this->belongsTo('App\Country');
+    }
+
+    public function category()
+    {
+        return $this->belongsToMany('App\Category');
+    }
 }
--- app/Country.php
+++ app/Country.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Country extends Model
 {
-    //
+    public function athlete()
+    {
+        $this->hasMany('App\Athlete');
+    }
 }
